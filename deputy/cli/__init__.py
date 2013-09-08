939f8664a78d895e1f34a331ab15da7143acebca
Aubrey Taylor <aubricus@gmail.com>
2013-09-08 13:10:48 -0700

    Minor edits.

--- deputy/cli/__init__.py
+++ deputy/cli/__init__.py
@@ -6,18 +6,20 @@ Usage: deputy [-v | --version]
 options:
     -h, --help           print deputy help message
     -v, --version        print deputy version
+
 """
 
 import sys
 import importlib
+import os
+import subprocess
 
 from docopt import docopt
 
 
 def main():
-    # TODO: Hardcoded - Make this dynaic
-    # This will probably be the plugin register / discovery step.
-    commands = ['uname']
+    commands = get_commands()
+    print(commands)
 
     args = docopt(
         __doc__,
@@ -30,9 +32,30 @@ def main():
     command_argv = [command] + command_args
 
     if command in commands:
-        result = importlib.import_module('deputy.commands.' + command)
-        result.exe(command_argv)
+        command_module = importlib.import_module('depfile.' + command)
+        print(command_module.__doc__)
+        command_module.exe(command_argv)
+    else:
+        print("'{}' is not a registered deputy.py command.\n".format(command))
+        sys.exit(subprocess.call(['deputy', '--help']))
 
+
+def get_commands():
+    cwd = os.getcwd()
+    plugin_dir = cwd + '/depfile'
+    plugins = []
+
+    # Add current working directory to PATH
+    if cwd not in sys.path:
+        sys.path.insert(0, cwd)
+
+    for i in os.listdir(plugin_dir):
+        if(i.endswith('.pyc') or i == "__init__.py"):
+            continue
+
+        plugins.append(i.strip('.py'))
+
+    return plugins
 
 if __name__ == "__main__":
     sys.exit(main())
