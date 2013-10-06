4beb24768a761e1f4d36f98b0e6128fdb9a1d768
Aubrey Taylor <aubricus@gmail.com>
2013-10-06 00:55:41 -0700

    Cleanup, setup.py

--- setup.py
+++ setup.py
@@ -5,51 +5,63 @@ from setuptools import setup
 
 from deputy import utils as deputy_utils
 
+# Setup command helpers
+
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
     sys.exit()
 
 os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
 
-package_data = {'deputy': ['LICENSE.md', 'README.md']}
+# Setup arg values
+
+url              = 'http://github.com/aubricus/deputy'
+name             = 'deputy'
+version          = deputy_utils.get_version()
+description      = 'A person whose immediate superior is a senior figure within an organization and who is empowered to act as a substitute for this superior.'
+author           = 'Aubrey Taylor'
+author_email     = 'aubricus@gmail.com'
+packages         = ['deputy']
+package_data     = {'deputy': ['LICENSE.md', 'README.md']}
+package_dir      = {'deputy': 'deputy'}
+long_description = open('README.rst').read()
+zip_safe         = False
 
 requires = [
     'setuptools',
     'docopt==0.6.1'
 ]
-
 entry_points = {
     'console_scripts': [
         'deputy = deputy.cli:main'
     ],
     'deputy.casefiles': []
 }
-
-description = 'A person whose immediate superior is a senior figure within an organization and who is empowered to act as a substitute for this superior.'
+classifiers=(
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+)
 
 setup(
-    url='http://github.com/aubricus/deputy',
-    name='deputy',
+    url=url,
+    name=name,
     version=deputy_utils.get_version(),
     description=description,
-    author='Aubrey Taylor',
-    author_email='aubricus@gmail.com',
-    packages=['deputy'],
+    author=author,
+    author_email=author_email,
+    packages=packages,
     package_data=package_data,
     install_requires=requires,
     entry_points=entry_points,
-    package_dir={'deputy': 'deputy'},
-    long_description=open('README.rst').read(),
-    classifiers=(
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ),
-    zip_safe=False,
+    package_dir=package_dir,
+    long_description=long_description,
+    classifiers=classifiers,
+    zip_safe=zip_safe,
 )
 
 del os.environ['PYTHONDONTWRITEBYTECODE']
